2de79310e0722cb101bca01e981fdf3af7a67dd4
Sean Ng <seanywng@users.noreply.github.com>
2026-04-10 15:15:02 +0800

    draft2

--- maybe_later.R
+++ maybe_later.R
@@ -1,8 +1,6 @@
 Malaysia, famous for its cash handouts, should focus on disbursing cash regularly to its poorest citizens. Blanket distributions such as myKasih are wasteful in that the value of a ringgit is much different to those under the poverty line than those above it. 
 
-<br>
-  
-  ```{r}
+
 census_fed |> 
   ggplot(aes(x = population_density, y = poverty_incidence)) + 
   geom_point() + 
@@ -27,15 +25,11 @@ census_fed |>
   
   plot_annotation(title = "Inverse relationship between gini and population density", 
                   subtitle = "R-squared of 0.28 amongst federal consituencies and 0.20 amongst state ones")
-```
-<br>
+
   
   Below, we observe the relationship between Gini (economic inequality, where 0 is perfect equality and 1 is where one person has all the income). 
 
 
-<br>
-  
-  ```{r}
 census_fed |> 
   ggplot(aes(x = population_density, y = gini)) + 
   geom_point() + 
@@ -57,14 +51,7 @@ census_fed |>
   
   plot_annotation(title = "Very weak relationship between gini and population density", 
                   subtitle = "R-squared of 0.03 amongst federal constituencies and 0.04 amongst state constituencies")
-```
 
-<br>
-  
-  
-  
-  
-  
   
   
   
@@ -73,7 +60,7 @@ census_fed |>
   
   
   
-  Irrespective of this, grievances persist,
+Irrespective of this, grievances persist,
 
 That the government would rather embrace further malapportionment than allow for greater federalism is telling
 
@@ -83,21 +70,20 @@ Perhaps one should look to politicians
 
 
 
-```{r}
+
 ballots |> 
   filter(election == "GE-15" & federal == "Federal" & 
            str_detect(state, "Labuan|Sabah|Sarawk")) |> 
   filter(result == "won") |> 
   arrange(votes)
-```
+
 
 
 Federalism 
 Neglect from the federal government 
 Not enough autonomy or economic development 
 
-
-```{r median-age-candidates}
+median-age-candidates
 
 ballots |> 
   mutate(year = year(date), 
@@ -126,4 +112,51 @@ ballots |>
        y = "Median age of candidates", 
        title = "Median age of candidates in Federal and State elections", 
        subtitle = "Median age of general population in yellow")
-```
+
+
+census_dun |> 
+  mutate(population_poor = poverty_incidence / 100 * population_total, 
+         east_malaysia = ifelse(
+           str_detect(state, "Labuan|Sabah|Sarawak"), 
+           "East Malaysia", 
+           "Peninsula"
+         )) |> 
+  group_by(
+    state, parlimen, east_malaysia
+  ) |> 
+  summarise(population_total = sum(population_total), 
+            nationality_non_citizen = sum(nationality_non_citizen),
+            population_poor = sum(population_poor),
+            .groups = "drop") |> 
+  mutate(
+    non_citizen_pc = nationality_non_citizen / population_total, 
+    poverty_incidence = population_poor / population_total
+  ) |> 
+  mutate(label = ifelse(
+    non_citizen_pc > .15 | poverty_incidence > .15, 
+    str_sub(parlimen, start = 6L), 
+    ""
+  )) |> 
+  arrange(desc(poverty_incidence)) |> 
+  ggplot(aes(x = non_citizen_pc, 
+             y = poverty_incidence)) + 
+  scale_x_continuous(labels = percent) + 
+  scale_y_continuous(labels = percent) +
+  geom_point(aes(colour = east_malaysia, 
+                 size = population_total), 
+             alpha = .7) + 
+  scale_colour_manual(values = c("#414487ff", "#7ad151ff")) +
+  scale_size_continuous(labels = comma) +
+  geom_text(aes(label = label), 
+            size = 2, 
+            check_overlap = FALSE) +  
+  labs(colour = "East Malaysia", 
+       x = "% Non-citizens", 
+       y = "Poverty incidence", 
+       size = "Total population", 
+       title = "The areas with the highest levels of poverty tend to be in East Malaysia") + 
+  guides(
+    size = guide_legend(override.aes = list(alpha = 1)), 
+    colour = guide_legend(override.aes = list(alpha = 1, 
+                                              size = 3))
+  )
